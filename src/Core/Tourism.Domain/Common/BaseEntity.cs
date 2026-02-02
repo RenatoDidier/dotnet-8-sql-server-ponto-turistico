8408b707f7d0989481abc10ddfbc310eda6dcca7
RenatoDidier <renatodidierf@gmail.com>
2026-02-02 07:35:37 -0300

    feat: Configuração do EF Core com a criação da primeira tabela e uma PROC de Create

---
+++ src/Core/Tourism.Domain/Common/BaseEntity.cs
@@ -0,0 +1,23 @@
+﻿namespace Tourism.Domain.Common;
+
+public class BaseEntity
+{
+    public Guid Id { get; protected set; }
+    public string Title { get; protected set; } = default!;
+    public DateTime CreatedAt { get; protected set; }
+    public DateTime? UpdatedAt { get; protected set; }
+    
+    protected BaseEntity() { }
+    protected BaseEntity(Guid id, string title)
+    {
+        Id = id;
+        Title = title;
+        CreatedAt = DateTime.UtcNow;
+    }
+
+    public void UpdateTitle(string title)
+    {
+        Title = title;
+        UpdatedAt = DateTime.UtcNow;
+    }
+}
---
+++ src/External/Tourism.Infrastructure/Contexts/TourismDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tourism.Domain.Entities;
+
+namespace Tourism.Infrastructure.Contexts;
+
+public class TourismDbContext : DbContext
+{
+    public TourismDbContext(DbContextOptions<TourismDbContext> options)
+    : base(options) { }
+
+    public DbSet<TouristAttractionEntity> TouristAttraction => Set<TouristAttractionEntity>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(TourismDbContext).Assembly);
+    }
+}
